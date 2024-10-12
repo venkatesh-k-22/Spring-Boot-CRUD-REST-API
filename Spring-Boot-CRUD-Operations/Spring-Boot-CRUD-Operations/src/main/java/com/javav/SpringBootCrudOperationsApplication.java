0e4f1d95926675e297212048d5ade81e257a6ee0
Venkatesh K <venkateshk14046@gmail.com>
2024-10-12 12:05:07 +0530

    Spring-Boot-CRUD-Operations Project Structure added

---
+++ Spring-Boot-CRUD-Operations/Spring-Boot-CRUD-Operations/src/main/java/com/javav/SpringBootCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.javav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOperationsApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-CRUD-Operations/Spring-Boot-CRUD-Operations/src/test/java/com/javav/SpringBootCrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
